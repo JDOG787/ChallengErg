7d376920c1aac370927128fe120ba45edcb230db
Jack Hendricks <64325143+JDOG787@users.noreply.github.com>
2022-03-16 22:45:17 -0700

    basic server

---
+++ api/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+
+app.post("/user/signup", async (req, res) => {
+    const token = await jwt.sign({username: "FART", id: 2}, "SECRET");
+    res.json({success: true, token});
+})
+
+app.listen(8080)
