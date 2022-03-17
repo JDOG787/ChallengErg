49727feb8f2dab1d1639c052a7bcf54ef20b7f0b
Jack Hendricks <64325143+JDOG787@users.noreply.github.com>
2022-03-16 22:47:09 -0700

    idk

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 
 app.post("/user/signup", async (req, res) => {
-    const token = await jwt.sign({username: "FART", id: 2}, "SECRET");
+    const token = await jwt.sign({username: "JDOG", id: 2}, "SECRET");
     res.json({success: true, token});
 })
 
